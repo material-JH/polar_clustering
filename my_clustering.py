272936de12e7064f952242009e7d45ff490e4e05
material-JH <126924889+material-JH@users.noreply.github.com>
2023-03-11 11:41:20 +0900

    w/o
    w./o

--- my_clustering.py
+++ my_clustering.py
@@ -5,10 +5,6 @@ import matplotlib.pyplot as plt
 import umap
 from sklearn.cluster import KMeans
 from sklearn.cluster import MiniBatchKMeans
-from sklearn.cluster import SpectralClustering
-from collections import Counter
-from matplotlib import cm
-from PIL import Image
 import imutils as imutils
 import matplotlib
 from scipy.spatial.distance import pdist, squareform
@@ -53,7 +49,7 @@ def get_rotation_matrix(i_v, unit=None):
     )
 
 def NormalizeData(data):
-    return (data - np.mean(data)) / data.
+    return (data - np.mean(data)) / np.std(data)
 
 
 X = []
@@ -64,23 +60,77 @@ X = np.stack(X, axis=0)
 
 X = X.reshape((-1, X.shape[1] * X.shape[2]))
 
+X = NormalizeData(X)
+
 #%%
 # Choose UMAP parameters
 n_neighbors = 100
 n_components = 3
 min_dist = 0.1
 
-embedding = umap.UMAP(n_neighbors=n_neighbors, n_components=n_components, min_dist=min_dist, random_state=42).fit_transform(X)
+reducer = umap.UMAP(n_components=2)
+embedding = reducer.fit_transform(X)
 
-# Visualize the results
-# fig, ax = plt.subplots(figsize=(10, 10))
-# ax.scatter(embedding[:, 0], embedding[:, 1], cmap='Spectral', s=10)
-# plt.setp(ax, xticks=[], yticks=[])
-# plt.title("UMAP projection of MNIST dataset")
-# plt.show()# %%
+from sklearn.cluster import KMeans
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.scatter(embedding[:,0],embedding[:,1],embedding[:,2])
+# Assuming `embedding` contains the reduced dimensional representation of your data
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(embedding)
+
+# Assign cluster labels to each data point
+labels = kmeans.labels_
+import matplotlib.pyplot as plt
+
+# Assuming `embedding` and `labels` have been obtained as shown in the previous code snippet
+# Plot the scatter plot with the first cluster in blue and the second cluster in red
+plt.scatter(embedding[labels == 0, 0], embedding[labels == 0, 1], c='blue', label='Cluster 1')
+plt.scatter(embedding[labels == 1, 0], embedding[labels == 1, 1], c='red', label='Cluster 2')
+
+plt.title('UMAP + K-means clustering')
+plt.legend()
+plt.show()
+
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+import umap
+from sklearn.cluster import KMeans
+
+# Assuming `X` is a numpy array with your data
+# Compute the UMAP embedding
+umap_model = umap.UMAP()
+embedding = umap_model.fit_transform(X)
+
+# Compute the K-means clusters
+kmeans_model = KMeans(n_clusters=2)
+labels = kmeans_model.fit_predict(X)
+
+# Compute the pairwise distances between the data points
+distances = np.linalg.norm(X[:, np.newaxis, :] - X[np.newaxis, :, :], axis=-1)
+
+# Plot the scatter plot with the K-means clusters and scaled markers based on the distances
+plt.scatter(embedding[:, 0], embedding[:, 1], c=labels,
+            s=1 + distances.flatten() / np.max(distances) * 100)
+plt.colorbar()
+plt.title('UMAP embedding with K-means clustering and scaled markers based on distances')
+plt.show()
+
+
+# %%
+distances = np.linalg.norm(X[:, np.newaxis, :] - X[np.newaxis, :, :], axis=-1)
+
+# Compute the mean distances for each cluster
+mean_distances = []
+for i in range(len(np.unique(labels))):
+    mean_distance = np.mean(distances[labels == i])
+    mean_distances.append(mean_distance)
+
+scaled_sizes = 1 + distances.flatten() / np.max(mean_distances) * 100
+for i in range(len(np.unique(labels))):
+    scaled_sizes[labels == i] /= mean_distances[i]
+
+print(scaled_sizes.shape)
+# %%
+plt.plot(distances[:,0])
 plt.show()
 # %%
