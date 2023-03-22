45a8e443bab452d7de83acf6358a47d86e1ade15
jinho93 <a94041404>
2023-03-22 13:28:15 +0900

    w/o comment

--- disk_generation_sim.py
+++ disk_generation_sim.py
@@ -12,17 +12,19 @@ for file in os.listdir('output'):
         arr.append(np.load(f'output/{file}'))
 
 for n in range(len(arr)):
-    arr[n] = resize(arr[n][0,0],  [r + 30 for r in arr[n][0,0].shape])
-# start_pos = [7, 101]
-start_pos = [55, 147]
+    arr[n] = resize(arr[n][0,0],  [r + 300 for r in arr[n][0,0].shape])
+#%%
+# start_pos = [55, 147]
+start_pos = [142, 236]
 rad = 50
 
 disk = []
 for n in range(len(arr)):
     disk.append(arr[n][start_pos[0]:start_pos[0] + rad,
                        start_pos[1]:start_pos[1] + rad])
+    plt.imshow(disk[n])
+    plt.show()
 center_of_mass_position(disk[0])
-plt.imshow(disk[0])
 # %%
-np.save('output/disk_011.npy', np.array(disk))
+np.save('output/disk_002.npy', np.array(disk))
 # %%
